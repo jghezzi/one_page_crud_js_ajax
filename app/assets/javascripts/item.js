6371f6761c422b8cf702fc3694e962854d14ac31
Jason Ghezzi <jasonghezzi@gmail.com>
2014-05-18 13:31:54 -0400

    all CRUD working in ajax

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,13 +1,62 @@
 $ (function() {
 
-	$('.update-class').on('click', function(){
+	$('.update-class').on('click', function(e){
 		var update_id = this.id;
 		console.log(update_id);
+		$('.update-modal').show("slow");
+		
+		$('.update-button').on('click', function(e){
+			console.log("MODAL UPDATE");
+			var item_to_update = $('#items_name').val()
+			console.log(item_to_update)
+			e.preventDefault;
+
+			$.ajax({
+		
+			url: 		"/items/" + update_id,
+			type: 	'PATCH',
+				data: 	{
+					item:  {
+						"name":  item_to_update
+					}
+				},
+				success: function(data){
+				console.log(data);
+				if (data == "1") {
+					console.log("true");
+					window.location.reload();	
+				}
+				else {
+					console.log("false");
+				}
+			}
+		})
+
+		});
+
 	});
 
 	$('.delete-class').on('click', function(){
 		var delete_id = this.id;
 		console.log(delete_id);
+
+		$.ajax({
+
+		url: "/items/" + delete_id,
+		type: "DELETE",
+
+		success: function(data){
+				console.log(data);
+				if (data == "1") {
+					console.log("true");
+					window.location.reload();	
+				}
+				else {
+					console.log("false");
+				}
+			}
+		})
+	
 	});
 
 	$('.add-class').on('click', function(){
@@ -17,28 +66,30 @@ $ (function() {
 
 	$('.add-button').on('click',function(e){
 		console.log("MODAL ADD")
+		$('.add-modal').hide("slow");
 		var my_new_item = $('#item_name').val()
 		console.log(my_new_item)
 		e.preventDefault();
 		$.ajax({
 		
-		url: 		"/items",
-		type: 	'POST',
-			data: 	{
-				item:  {
-					"name":  my_new_item
+			url: 		"/items",
+			type: 	'POST',
+				data: 	{
+					item:  {
+						"name":  my_new_item
+					}
+				},
+				success: function(data){
+				console.log(data);
+				if (data == "1") {
+					console.log("true");
+					window.location.reload();	
 				}
-			},
-			success: function(data){
-			console.log(data);
-			if (data == "1") {
-				console.log("true");
+				else {
+					console.log("false");
+				}
 			}
-			else {
-				console.log("false");
-			}
-		}
 		})
 	})
-	
+
 });
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,31 @@ class ItemsController < ApplicationController
 	def index
 		@items = Item.all
 		@new_item = Item.new
+		@item = Item.all
 	end
+
+	def edit
+  	@items = Item.all
+  	@item = Item.find(params[:id])
+  	render :action => "index"
+	end
+
+	def update
+	  @items = Item.all
+	  @item = Item.find(params[:id])
+	  if @item.update_attributes(item_params)
+	    respond_to do |format|
+				format.js
+				format.html { redirect_to items_path }
+			end
+		else
+			respond_to do |format|
+				format.js { render plain: "0" }
+				format.html { redirect_to lists_path, notice: "Update failed." }
+			end
+		end
+	end
+
 
 	def create
 		@new_item = Item.new(item_params)
@@ -24,6 +48,21 @@ respond_to do |format|
 				format.js
 				format.html { redirect_to lists_path }
 			end
+
+	def destroy
+		@item = Item.find(params[:id])
+		if @item.destroy
+				respond_to do |format|
+					format.js
+					format.html { redirect_to items_path }
+				end
+		else
+			respond_to do |format|
+				format.js { render plain: "0" }
+				format.html { redirect_to lists_path, notice: "Destroy failed." }
+			end
+		end
+	end
 
 
 	private
